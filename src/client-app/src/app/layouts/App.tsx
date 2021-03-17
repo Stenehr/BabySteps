0b95daf574a83001c106a77fea0db95e859f9992
stenehr <sten.ehrberg@gmail.com>
2021-03-17 14:59:15 -0700

    setting up client-app

---
+++ src/client-app/src/app/layouts/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Button color="red">TEST</Button>
+    </div>
+  );
+}
+
+export default App;
